56dc9fccc0f64b23fb1073ad36babc43b12cebda
Flavio721 <flavio2010sjc@gmail.com>
2025-12-05 19:43:15 -0300

    Configurando o database

--- scripts/database.php
+++ scripts/database.php
@@ -1,8 +1,8 @@
 <?php
-$dbHost = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "nieta";
+$dbHost = "sql107.infinityfree.com";
+$dbUsername = "if0_40610354";
+$dbPassword = "Djalma53";
+$dbName = "if0_40610354_XXX";
 
 $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 ?>
