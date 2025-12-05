a513396dd59cd8dfcfa8a6ee3deeb3c9c0b735d7
Flavio721 <flavio2010sjc@gmail.com>
2025-12-05 19:54:47 -0300

    Alterando o código PHP

--- scripts/login.php
+++ scripts/login.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://site-nieta.vercel.app");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 include_once('database.php');
